35fc3399263f7804ceb435f8190cab0267d42582
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-23 13:28:01 -0800

    ⚡ Init repo

--- k9_analysis/__init__.py
+++ k9_analysis/__init__.py
@@ -1,4 +1,4 @@
 """A template Python module"""
 
 __version__ = "0.4.0"
-from .transformations import add_offset  # noqa
+from .dummy import add_offset  # noqa
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,4 @@
-import dummy_transformations as dt
+import k9_analysis as dt
 
 
 def test_add_offset():
