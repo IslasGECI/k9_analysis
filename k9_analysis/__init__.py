154e147c22beeb4cc70535089560cff4ece6dbd9
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-31 11:50:33 -0800

    😇 Pass test

--- k9_analysis/__init__.py
+++ k9_analysis/__init__.py
@@ -1,5 +1,4 @@
 """A module to do analyses of k9 data"""
 
 __version__ = "0.4.1"
-from .dummy import add_offset  # noqa
 from .count_burrows_detection import *  # noqa
--- tests/test_count_burrows_detection.py
+++ tests/test_count_burrows_detection.py
@@ -1,5 +1,9 @@
+from k9_analysis import count_nest
 import pandas as pd
 k9_data = pd.read_csv("tests/data/registros_rastros_de_gatos_k9_guadalupe_ISO8601.csv")
 
 def test_count_nest():
-    count_nest()
+    obtained = count_nest(k9_data)
+    obtained_type = type(obtained)
+    expected_type = "integer"
+    assert obtained_type == expected_type
