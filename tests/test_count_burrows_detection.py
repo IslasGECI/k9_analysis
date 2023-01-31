ebe464edcd1aa4a9830147e0539dc9abb2ecdff4
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-31 12:19:57 -0800

    🛑🧪 Test 'filter_dates()'

--- tests/test_count_burrows_detection.py
+++ tests/test_count_burrows_detection.py
@@ -1,4 +1,4 @@
-from k9_analysis import count_nest
+from k9_analysis import count_nest, filter_dates
 import pandas as pd
 
 k9_data = pd.read_csv("tests/data/registros_rastros_de_gatos_k9_guadalupe_ISO8601.csv")
@@ -9,3 +9,9 @@ def test_count_nest():
     obtained_type = type(obtained)
     expected_type = int
     assert obtained_type == expected_type
+
+def test_filter_dates():
+    data = pd.DataFrame(
+        {"Fecha": ["2022-12-01", "2022-12-31", "2023-01-31"],
+        "Nombre_k9": ["Maya", "Maya", "Maya"]})
+    filter_dates(data)
