4a62c6737de9176573c8c3881dfb99f9abe68da5
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-31 12:50:02 -0800

    💥🧪 Failing test for 'filter_k9()'

--- tests/test_count_burrows_detection.py
+++ tests/test_count_burrows_detection.py
@@ -1,4 +1,4 @@
-from k9_analysis import count_nest, filter_dates
+from k9_analysis import count_nest, filter_dates, filter_k9
 import pandas as pd
 
 k9_data = pd.read_csv("tests/data/registros_rastros_de_gatos_k9_guadalupe_ISO8601.csv")
@@ -24,3 +24,10 @@ def test_filter_dates():
     obtained_dates = obtained.Fecha
     expected_dates = ["2022-12-01", "2022-12-31"]
     assert (obtained_dates == expected_dates).all()
+
+
+def test_filter_k9():
+    data = pd.DataFrame(
+        {"Fecha": ["2022-12-01", "2022-12-31", "2023-01-31"], "Nombre_k9": ["Maya", "Thor", "Maya"]}
+    )
+    obtained = filter_k9("Maya", data)
