8c88fd2353e5f1dda90066f994fb54674263af36
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-31 11:45:56 -0800

    🪛Improve description

--- k9_analysis/__init__.py
+++ k9_analysis/__init__.py
@@ -1,4 +1,5 @@
-"""A template Python module"""
+"""A module to do analyses of k9 data"""
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 from .dummy import add_offset  # noqa
+from .count_burrows_detection import *  # noqa
