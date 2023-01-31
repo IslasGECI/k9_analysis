fc9750928d965b7e9c6fba5c8bbace035708b66d
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-31 13:29:23 -0800

    ✅ Pass test

--- k9_analysis/count_burrows_detection.py
+++ k9_analysis/count_burrows_detection.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 
 def count_nest(k9_data) -> int:
-    return 0
+    start_date = "2022-01-01"
+    end_date = "2023-01-29"
+    k9_name = "Maya"
+    only_maya = filter_k9(k9_name, k9_data)
+    number_of_nest = filter_dates(only_maya, start_date, end_date)
+    return number_of_nest
 
 
 def filter_dates(k9_data: pd.DataFrame, start_date: str, end_date: str) -> pd.DataFrame:
@@ -12,3 +17,7 @@ def filter_dates(k9_data: pd.DataFrame, start_date: str, end_date: str) -> pd.Da
 
 def filter_k9(k9_name: str, k9_data: pd.DataFrame) -> pd.DataFrame:
     return k9_data.loc[k9_data.Nombre_k9 == k9_name]
+
+
+def filter_nest(k9_data: pd.DataFrame) -> pd.DataFrame:
+    return k9_data.loc[k9_data.Tipo_de_rastro == "MD"]
