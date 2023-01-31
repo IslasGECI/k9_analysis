97bea45427030571748b50e9a0c953cbd0c2d09e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-31 12:12:55 -0800

    🪲✅Fix type

--- tests/test_count_burrows_detection.py
+++ tests/test_count_burrows_detection.py
@@ -1,9 +1,11 @@
 from k9_analysis import count_nest
 import pandas as pd
+
 k9_data = pd.read_csv("tests/data/registros_rastros_de_gatos_k9_guadalupe_ISO8601.csv")
+
 
 def test_count_nest():
     obtained = count_nest(k9_data)
     obtained_type = type(obtained)
-    expected_type = "integer"
+    expected_type = int
     assert obtained_type == expected_type
