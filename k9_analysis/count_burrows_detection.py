c78ecccc5a8172963e7676931a72ff2b4973bc3b
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-31 12:51:43 -0800

    ✅ Pass test for 'filter_k9()'

--- k9_analysis/count_burrows_detection.py
+++ k9_analysis/count_burrows_detection.py
@@ -8,3 +8,7 @@ def count_nest(k9_data) -> int:
 def filter_dates(k9_data: pd.DataFrame, start_date: str, end_date: str) -> pd.DataFrame:
     filtered_df = k9_data.loc[(k9_data.Fecha <= end_date) & (k9_data.Fecha >= start_date)]
     return filtered_df
+
+
+def filter_k9(k9_name: str, k9_data: pd.DataFrame) -> pd.DataFrame:
+    pass
