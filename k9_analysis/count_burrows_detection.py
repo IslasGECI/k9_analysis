58b9dedfe6dadbdaf862b84cc6e5ba15edc4335e
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-31 16:23:32 -0800

    🏹👾 Force datetime column

--- k9_analysis/count_burrows_detection.py
+++ k9_analysis/count_burrows_detection.py
@@ -20,6 +20,7 @@ def count_nest(k9_data) -> int:
 
 
 def filter_dates(k9_data: pd.DataFrame, start_date: str, end_date: str) -> pd.DataFrame:
+    k9_data["Fecha"] = pd.to_datetime(k9_data["Fecha"], format="%Y-%m-%d")
     filtered_df = k9_data.loc[(k9_data.Fecha <= end_date) & (k9_data.Fecha >= start_date)]
     return filtered_df
 
