290f16c0bc69db9222ab698be34253d1b26350c5
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-02-01 10:11:13 -0800

    ✨🖨️ Add 'write_maya_nests_table()'

---
+++ tests/test_clis.py
@@ -0,0 +1,17 @@
+from k9_analysis import write_maya_nests_table
+
+import os
+
+
+def test_write_maya_nests_table():
+    input_path = "tests/data/registros_rastros_de_gatos_k9_guadalupe_ISO8601.csv"
+    output_path = "tests/data/maya_nests_table.csv"
+    start_date = "2022-01-01"
+    end_date = "2023-01-29"
+    write_maya_nests_table(start_date, end_date, input_path, output_path)
+    assert_path_exists(output_path)
+
+
+def assert_path_exists(submission_path):
+    assert os.path.exists(submission_path)
+    os.remove(submission_path)
