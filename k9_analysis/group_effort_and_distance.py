004b3df0cec7e28decd281803172e0905c9f5dc3
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-02-10 13:24:35 -0800

    🚚 Move function

--- k9_analysis/group_effort_and_distance.py
+++ k9_analysis/group_effort_and_distance.py
@@ -14,3 +14,9 @@ def interval_in_hours(string_time: str):
 def add_duration_in_hours_column(effort_data: pd.DataFrame):
     effort_data["Duration_hr"] = effort_data.Duracion.apply(interval_in_hours)
     return effort_data
+
+
+def make_summary_of_effort_and_distance(effort_df: pd.DataFrame):
+    column_added = add_duration_in_hours_column(effort_df)
+    grouped_dataframe = column_added.groupby("Nombre_k9")[["Distancia", "Duration_hr"]].aggregate("sum")
+    return grouped_dataframe.rename(columns={"Distancia": "Total_distance", "Duration_hr": "Total_time"})
