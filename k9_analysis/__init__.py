25b830fe734f2b78d296b7e51601b952b988ce4f
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-02-20 11:52:55 -0800

    🆙 Upgrade minor version

--- k9_analysis/__init__.py
+++ k9_analysis/__init__.py
@@ -1,6 +1,6 @@
 """A module to do analyses of k9 data"""
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 from .count_burrows_detection import *  # noqa
 from .write_maya_nests_table import *  # noqa
 from .ReportingEffort import *  # noqa
