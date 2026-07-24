8358bca3f5d8ef957a440ebfdf897e87ba48ccb7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-24 18:02:37 +0000

    ♻️ Refactor

--- k9_analysis/ReportingEffort.py
+++ k9_analysis/ReportingEffort.py
@@ -13,8 +13,9 @@ class ReportingEffort:
         column_name = "Total_distance"
         return self.get_maya_value_from_column(column_name)
 
+    def get_maya_effort_time(self):
+        column_name = "Total_time"
+        return self.get_maya_value_from_column(column_name)
+
     def get_maya_value_from_column(self, column_name):
         return int(self.effort_k9.loc[self.effort_k9["k9_name"] == "Maya", column_name].item())
-
-    def get_maya_effort_time(self):
-        return int(self.effort_k9.loc[self.effort_k9["k9_name"] == "Maya", "Total_time"].item())
