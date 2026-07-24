1f7cf2f961e93e9e2b9bf3590eb41002a0d10c76
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2026-07-24 14:25:42 -0700

    🆙🏷️ forced upgrade patch number version

--- k9_analysis/__init__.py
+++ k9_analysis/__init__.py
@@ -1,6 +1,6 @@
 """A module to do analyses of k9 data"""
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 from .count_burrows_detection import *  # noqa
 from .cli import *  # noqa
 from .ReportingEffort import *  # noqa
